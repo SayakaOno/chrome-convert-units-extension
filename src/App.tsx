5e52d525db6a2978cbdab7056d3587b99bb221fa
Sayaka Ono <sayaka.ono.81@gmail.com>
2023-03-25 19:36:42 -0700

    add UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ChangeEvent } from "react";
+import "./App.css";
+import Box from "@mui/material/Box";
+import FormControl from "@mui/material/FormControl";
+import { SelectChangeEvent } from "@mui/material/Select";
+import TextField from "@mui/material/TextField";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputAdornment from "@mui/material/InputAdornment";
+import Autocomplete from "@mui/material/Autocomplete";
+
+const units = ["mm", "cm", "m"];
 
 function App() {
+  const [value, setValue] = React.useState("");
+  const [unit, setUnit] = React.useState("");
+
+  const handleChange = (
+    event: SelectChangeEvent | ChangeEvent<HTMLTextAreaElement | HTMLInputElement>,
+    set: (value: string) => void,
+  ) => {
+    set(event.target.value as string);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: "flex" }}>
+      <div>
+        <TextField
+          label="Value"
+          id="outlined-size-small"
+          size="small"
+          type={"number"}
+          value={value}
+          onChange={(e) => handleChange(e, setValue)}
+        />
+      </div>
+      <Box sx={{ width: 100, marginRight: "10px" }}>
+        <Autocomplete
+          disablePortal
+          id="combo-box-demo"
+          size="small"
+          value={unit}
+          options={units}
+          sx={{ width: 100 }}
+          renderInput={(params) => (
+            <TextField {...params} label="Unit" onChange={(e) => handleChange(e, setUnit)} />
+          )}
+        />
+      </Box>
+      <FormControl variant="outlined">
+        <OutlinedInput
+          id="outlined-adornment-weight"
+          size="small"
+          disabled
+          endAdornment={<InputAdornment position="end">kg</InputAdornment>}
+          aria-describedby="outlined-weight-helper-text"
+          inputProps={{
+            "aria-label": "weight",
+          }}
+          style={{ width: 100 }}
+        />
+      </FormControl>
     </div>
   );
 }
