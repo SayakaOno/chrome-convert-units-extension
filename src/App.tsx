ac03f85f14a697c2079dd59fbb7a5099867f5b91
Sayaka Ono <sayaka.ono.81@gmail.com>
2023-03-25 22:34:14 -0700

    update select option padding

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { ChangeEvent, useEffect, useMemo } from "react";
 import { SelectChangeEvent } from "@mui/material/Select";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
+import Paper from "@mui/material/Paper";
 import { getUnitSet, isValidUnit, convert } from "./utils";
 import { allUnits, Unit } from "./constants";
 import "./App.css";
@@ -67,20 +68,32 @@ function App() {
           value={value}
           onChange={(e) => handleChange(e, setValue)}
         />
-        <Autocomplete
-          disablePortal
-          id="combo-box-demo"
-          size="small"
-          options={allUnits}
-          sx={{ width: 200 }}
-          value={unit}
-          onChange={(event: React.SyntheticEvent<Element, Event>, newValue: string | null) => {
-            setUnit((newValue as Unit) || "");
+        <Paper
+          sx={{
+            boxShadow: "none",
+            "& .MuiAutocomplete-option": {
+              paddingTop: "2px!important",
+              paddingBottom: "2px!important",
+            },
           }}
-          renderInput={(params) => (
-            <TextField {...params} label="Unit" onChange={handleTextFieldChange} />
-          )}
-        />
+        >
+          <Autocomplete
+            disablePortal
+            id="combo-box-demo"
+            size="small"
+            options={allUnits}
+            sx={{
+              width: 150,
+            }}
+            value={unit}
+            onChange={(event: React.SyntheticEvent<Element, Event>, newValue: string | null) => {
+              setUnit((newValue as Unit) || "");
+            }}
+            renderInput={(params) => (
+              <TextField {...params} label="Unit" onChange={handleTextFieldChange} />
+            )}
+          />
+        </Paper>
       </div>
       {convertedValues}
     </div>
